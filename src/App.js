d2ee686d4e5efdf2cc4bfb51ef5e5765eca8145c
lazygame2526 <pernabrannon@yahoo.com>
2023-08-18 12:46:32 -0700

    Tutorial 1:29

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import ToDo from './components/ToDo'
 
 
 function App() {
   return (
-    <div>Hello Charlie!</div>
+      <div>
+          <h1>My To do's</h1>
+          <ToDo text='Taco' />
+          <ToDo text='Burger' />
+          <ToDo text='Cake' />
+      </div>
   );
 }
 
